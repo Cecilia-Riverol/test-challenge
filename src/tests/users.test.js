68270d5c496c91621f03b1379b04aee1d9fad2ff
Cecilia-Riverol <sandra.ruverol@gmail.com>
2024-05-07 10:20:24 -0600

    Primer commit para la prueba de los usuarios

--- src/tests/users.test.js
+++ src/tests/users.test.js
@@ -1,9 +1,19 @@
-// You already have the methods imported
-import { default as expect } from "expect";
-import { getUser, getUsers } from "../users";
+const { buscarUser } = require('../users.js');
 
-// Make a test to verify if the method getUser(id) works correctly
-// Verify any data of the resulting object you want
-
-// READ documentation
-
+describe('Testing buscarUser function', () => {
+  it('should console.log user data for a valid user ID', async () => {
+    await buscarUser(1);
+    //Esperamos que nos de todo el resultado del array del usuario 1
+    expect({
+      id: 1,
+      username: "hpLover4",
+      email: "sirious90@gmail.com",
+      password: "hArrydotCom",
+    });
+  });
+  it('should console.error an error message for an invalid user ID', async () => {
+    await buscarUser(45);
+    //Esperamos que nos marque el erro cuando el dato del usario no exista
+    expect(Error("User does not exist, please try again"));
+  });
+});
--- src/users.js
+++ src/users.js
@@ -1,19 +1,33 @@
-// Imports small array of users
-// Pretend it's an API request
-const users = require("./data").default;
+// Importamos la infromación de la pàgina de datos
+const users = require("./data");
 
-// Fetches all users
-const getUsers = () => {
-  return users;
-};
-
-// Filters users by specific ID
+// Simulación como si fuese una API usando el método de promesa
 const getUser = (id) => {
-  return users.find((user) => user.id === id);
+  return new Promise((resolve, reject) => {
+    //Dentro del array User buscamos el ID y nos cercioramos de que sean identicos
+      const user = users.find((user) => user.id === id);
+
+      //Si el id del usuario es igual a lo que tenemos en la base de datos vamos a resolver, sino vamos a colocar que no existe y que intente de nuevo.
+      if (user) {
+        resolve(user);
+      } else {
+        reject(new Error("User does not exist, please try again"));
+      }
+  });
 };
 
-// test
-// console.log(getUser(3));
+//Función para encontrar un User basado en su ID. - La función arroja toda la información del User
+const buscarUser = async (id) => {
+  try {
+    const user = await getUser(id);
+    console.log(user); //Imprimimos la información del usuario
+  } catch (err) {
+    console.error(err); //Mnadamos el error
+  }
+};
 
-module.exports = { getUsers, getUser };
+// Pruebas para saber que está bien
+buscarUser(1); // FDebe regresar la información de hpLover
+buscarUser(99); // Debe regresar el mensaje de usuario inexistente
 
+module.exports = { buscarUser};
